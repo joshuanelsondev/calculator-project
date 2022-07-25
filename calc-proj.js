f26a19adc23aa7a45cf855a7a62c17006025ccba
Joshua Nelson <joshuanelson894@gmail.com>
2022-07-25 01:24:20 -0400

    Add function and array method to begin getting the calc to display
    update the wifi image

--- calc-proj.js
+++ calc-proj.js
@@ -1,7 +1,4 @@
 
-
-
-
 // Time function
 function time() {
     const today = new Date();
@@ -12,3 +9,50 @@ function time() {
     const time = hours + ':' + mins;
     document.getElementById('time').innerHTML = time;
 }
+
+let displayValue = 0;
+const display = document.getElementById('display');
+const buttons = Array.from(document.querySelectorAll('button'));
+
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch (e.target.value) {
+            case 'AC':
+                display.innerHTML = 0;
+                break;
+            case 
+
+
+
+            default:
+                display.innerHTML += e.target.value;
+        }
+    });
+}); 
+
+updateDisplay = () => {
+    
+    if(displayValue.length > 9) {
+        display.innerHTML = displayValue.substring(0,9);
+    }
+    if(displayValue != 0) {
+        display.innerHTML = displayValue;
+
+        
+    } else {
+        display.innerHTML = displayValue;
+    }
+};
+
+updateDisplay();
+
+clear = () => {
+
+};
+
+
+
+
+
+
